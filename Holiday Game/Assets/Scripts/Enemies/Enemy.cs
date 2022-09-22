a1ae42cdd7f3bc3eaa4d9cb1989cbca0c6bb1c16
Patrick Doherty <prd9410@rit.edu>
2022-09-22 01:11:05 -0400

    Updated Enemy Movement, Shooter now tries to stay within a specific range of player.

--- Holiday Game/Assets/Scripts/Enemies/Enemy.cs
+++ Holiday Game/Assets/Scripts/Enemies/Enemy.cs
@@ -10,11 +10,24 @@ public abstract class Enemy : StatsComponent
 
     protected Vector2 velocity = Vector2.zero;
 
+
+    /// <summary>
+    /// Minimum distance between enemy & player
+    /// </summary>
+    protected float minPlayerDist;
+
+    /// <summary>
+    /// Maximum distance between enemy & player
+    /// </summary>
+    protected float maxPlayerDist;
+
     //Which XP prefab this enemy will drop
     [SerializeField]
     public XP XPType;
 
-    // Moves the enemy towards the player
+    /// <summary>
+    /// Moves the Enemy towards the player
+    /// </summary>
     protected void seekPlayer()
     {
         // Get the players position
@@ -23,6 +36,50 @@ public abstract class Enemy : StatsComponent
         // Get the players position, seek that point, apply forces, and move
         velocity = desiredVelocity * Speed;
         GetComponent<Rigidbody2D>().velocity = velocity;
+    }
+
+    /// <summary>
+    /// Moves the Enemy away from the player
+    /// </summary>
+    protected void fleePlayer()
+    {
+        // Get Player Position
+        Vector2 desiredVelocity = (Vector2)transform.position - (Vector2)player.transform.position;
+
+        // Take the Player's position, flee from it and move
+        velocity = desiredVelocity * Speed;
+        GetComponent<Rigidbody2D>().velocity = velocity;
+    }
+
+    /// <summary>
+    /// Stays within a certain distance of the player, either fleeing or seeking
+    /// </summary>
+    protected void shooterMove()
+    {
+        // Calculate the distance & compare it to the values
+        if(playerDistance() >= maxPlayerDist)
+        {
+            // Player is too far away, move closer
+            seekPlayer();
+        }
+        else if(playerDistance() <= minPlayerDist)
+        {
+            //player is too close, move away
+            fleePlayer();
+        }
+        else
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }
+    }
+
+    /// <summary>
+    /// Calculates the distance between player & enemy
+    /// </summary>
+    /// <returns>Distance between</returns>
+    protected float playerDistance()
+    {
+        return Vector2.Distance((Vector2)player.transform.position, (Vector2)transform.position);
     }
 
     public override void OnDeath()
--- Holiday Game/Assets/Scripts/Enemies/TestEnemy2.cs
+++ Holiday Game/Assets/Scripts/Enemies/TestEnemy2.cs
@@ -6,11 +6,16 @@ public class TestEnemy2 : Enemy
 {
     public override void OnUpdate()
     {
+        shooterMove();
     }
 
     private void Start()
     {
         base.Start();
+
+        // Set player ranges
+        minPlayerDist = 6f;
+        maxPlayerDist = 8f;
 
         AddAttack(GameManager.instance.GetWeaponFromIndex(GameManager.WeaponIndex.Test));
     }

b807a5853b7b1e55c46c55d57a6c37eca579d499
Josh Clark (RIT Student) <joshtclark7@gmail.com>
2022-09-28 14:10:11 -0400

    Created the upgrade script

---
+++ Holiday Game/Assets/Scripts/Upgrade.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Upgrade Config", menuName = "ScriptableObject/Upgrade Config")]
+public class Upgrade : ScriptableObject
+{
+}

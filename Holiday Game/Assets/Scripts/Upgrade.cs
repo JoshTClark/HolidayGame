88740b93e056d48f276e919dd2b646290286bf00
Josh Clark (RIT Student) <joshtclark7@gmail.com>
2022-09-28 14:21:10 -0400

    Set up upgrades a bit further

--- Holiday Game/Assets/Scripts/Upgrade.cs
+++ Holiday Game/Assets/Scripts/Upgrade.cs
@@ -5,4 +5,17 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Upgrade Config", menuName = "ScriptableObject/Upgrade Config")]
 public class Upgrade : ScriptableObject
 {
+    public enum Tier
+    {
+        Common,
+        Uncommon,
+        Rare
+    }
+
+    public string upgradeName;
+    public string upgradeDescription;
+    public GameManager.UpgradeIndex index;
+    public Tier tier;
+    [Range(1,5)]
+    public int levels;
 }

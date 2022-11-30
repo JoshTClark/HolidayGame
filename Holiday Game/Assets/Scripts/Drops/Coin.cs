f29391b54ebf2028ac829e65c04215c3b5394086
djs6413 <djs6413@rit.edu>
2022-11-30 14:46:11 -0500

    Gold drop
    Added a gold drop

---
+++ Holiday Game/Assets/Scripts/Drops/Coin.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : DropBase
+{
+    public override void HandleCollision(Collider2D other)
+    {
+        //if colliding with player
+        if (other.gameObject.GetComponent<Player>())
+        {
+            //add 5 gold to total
+            GameManager.instance.GainGold(5);
+            //remove the health drop
+            pool.Release(this);
+        }
+    }
+}

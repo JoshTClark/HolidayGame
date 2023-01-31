4a5b6e4e81f4c6ab39eb87e2e77ee51003dbfafb
Josh Clark <joshtclark7@gmail.com>
2023-01-31 13:02:37 -0500

    Made player level up slower

--- Holiday Game/Assets/Scripts/Managers/GameManager.cs
+++ Holiday Game/Assets/Scripts/Managers/GameManager.cs
@@ -59,7 +59,6 @@ public class GameManager : MonoBehaviour
     private int upgradesToGive = 0;
     private float dayLength = 120f;
     public int currentDay = 1;
-    public int currentSeason = 0;
     public int currentHour = 12;
     private List<string> seasonsOrdered = new List<string>();
     public List<WeaponIcon> weaponIcons = new List<WeaponIcon>();
@@ -78,6 +77,11 @@ public class GameManager : MonoBehaviour
     public float GameTime
     {
         get { return time; }
+    }
+
+    public float OffsetTime
+    {
+        get { return time + (levelData.startHour * (dayLength/24)); }
     }
 
     public GameState State
@@ -192,7 +196,7 @@ public class GameManager : MonoBehaviour
                 UpdateDate();
 
                 // Updating displays
-                timerDisplay.text = seasonsOrdered[currentSeason] + ": Day " + currentDay;
+                timerDisplay.text = "Day " + currentDay;
                 dashText.text = player.currentDashes.ToString();
                 xpBar.GetComponent<RectTransform>().anchorMax = new Vector2(player.GetPercentToNextLevel(), xpBar.GetComponent<RectTransform>().anchorMax.y);
                 dashTimer.GetComponent<RectTransform>().anchorMax = new Vector2(1 - (player.dashCooldownTimer / player.DashCooldown), dashTimer.GetComponent<RectTransform>().anchorMax.y);
@@ -460,9 +464,8 @@ public class GameManager : MonoBehaviour
     */
     public void UpdateDate()
     {
-        currentSeason = (int)Mathf.Floor(time / (dayLength * 5));
-        currentDay = (int)Mathf.Floor((time - ((currentSeason - 1) * 5)) / dayLength) + 1;
-        currentHour = (int)((time % dayLength) / (dayLength / 24));
+        currentDay = (int)Mathf.Floor(time / dayLength) + 1;
+        currentHour = (int)((OffsetTime % dayLength) / (dayLength / 24));
 
         float percentThroughDay = (time % dayLength) / dayLength;
 
@@ -473,14 +476,14 @@ public class GameManager : MonoBehaviour
         dayBar3.rectTransform.anchorMin = new Vector2(2f - percentThroughDay - 0.5f, dayBar3.rectTransform.anchorMin.y);
         dayBar3.rectTransform.anchorMax = new Vector2(3f - percentThroughDay - 0.5f, dayBar3.rectTransform.anchorMax.y);
 
-        float currentHourFloat = ((time % dayLength) / (dayLength / 24));
+        float currentHourFloat = ((OffsetTime % dayLength) / (dayLength / 24));
         //globalLight.intensity = 0.1f;
         globalLight.intensity = Mathf.Clamp(1f - Mathf.Pow(Mathf.Abs(currentHourFloat - 12) / 12f, 3f / 4f) + (0.2f * (0.9f - Mathf.Abs(currentHourFloat - 12) / 12f)), 0f, 1f);
     }
 
     public bool IsDayLight()
     {
-        if (Mathf.RoundToInt((time - (dayLength / 4)) / dayLength) % 2 == 0)
+        if (Mathf.RoundToInt((OffsetTime - (dayLength / 4)) / dayLength) % 2 == 0)
         {
             return true;
         }
